6c49fdc923f33fc90a890322dd63e05bcde7364a
Kosuke Nagano <gm_kou@mac.com>
2012-02-07 01:56:13 +0900

    Modify to use express

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-var io = require('socket.io').listen(8888);
+var app = require('express').createServer();
+var io = require('socket.io').listen(app);
+
+app.listen(8888);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.get('/button', function (req, res) {
+  res.sendfile(__dirname + '/button.html');
+});
+app.get('/display', function (req, res) {
+  res.sendfile(__dirname + '/display.html');
+});
+
 io.sockets.on('connection', function (socket) {
     socket.on('myevent', function (data) {
 	socket.emit('myevent', data);
